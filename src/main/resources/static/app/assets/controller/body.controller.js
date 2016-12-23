7ef93731c17ff62206d335a041c71461ecefb589
matt <matt.ledbetter@gmail.com>
2016-12-22 22:50:05 -0500

    working on page layout and tab mouseover functions.

--- src/main/resources/static/app/assets/controller/body.controller.js
+++ src/main/resources/static/app/assets/controller/body.controller.js
@@ -3,13 +3,43 @@
 
 	angular.module('sampleApp').controller('BodyController', Body);
 
-	Body.$inject = [ '$scope', '$rootScope','$state','$window'];
+	Body.$inject = [ '$scope', '$state','$window', '$log', '$timeout'];
 
-	function Body($scope, $rootScope,$state,$window) {
-		
-		var vm = this;
+	function Body($scope, $state, $window, $log, $timeout) {
 
-		$scope.open = false;
+        var vm = this;
+        vm.showMenu = false;
+
+        $scope.open = false;
+
+        /**
+		 * What happens when a user hovers over tab.
+         * @param isHover
+         */
+        vm.setHover = function(isHover) {
+            vm.showMenu = isHover;
+        };
+
+        /**
+		 * What happens when a user mouse leaves a tab with a submenu.
+         * @param event
+         */
+        vm.leaveTab = function(event) {
+        	$log.info(event);
+		};
+
+        /**
+		 * What happens when a user mouse enters a tab with a submenu.
+         * @param event
+         */
+        vm.enterTab = function(event) {
+        	if (vm.timeout) {
+        		$timeout.cancel(vm.timeout);
+			}
+			vm.timeout = $timeout(function() {
+				$log.info(event.clientX + ", " + event.clientY);
+			}, 200);
+		};
 
 		function render()
 		{
