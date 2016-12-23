9f9d1bd95cd8861937ec334f488e7ec375283daf
asp5045 <alok_s_patel1@homedepot.com>
2016-12-23 07:54:54 -0500

    added checkout page design

--- src/main/resources/static/app/assets/app.js
+++ src/main/resources/static/app/assets/app.js
@@ -1,7 +1,6 @@
-﻿var app = angular.module('sampleApp', ['ui.bootstrap','ngSanitize','matchMedia','ui.router','uiSwitch','ui.mask','AngularPrint','chart.js','infinite-scroll','textAngular','angularSpectrumColorpicker']);
-  
-app.config(['$provide','$stateProvider','$urlRouterProvider','ChartJsProvider',function($provide,$stateProvider, $urlRouterProvider,ChartJsProvider){
+﻿var myApp = angular.module("mainModule",[]);
 
+excellWirellApp.controller("MainController",function()
+{
 
-  
-}]);
+});
