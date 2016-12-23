bccad8f04872455eb37fd3ee60e6326cd1d87147
matt <matt.ledbetter@gmail.com>
2016-12-23 08:45:15 -0500

    x y stuff working.

--- src/main/resources/static/app/assets/controller/body.controller.js
+++ src/main/resources/static/app/assets/controller/body.controller.js
@@ -3,14 +3,12 @@
 
 	angular.module('sampleApp').controller('BodyController', Body);
 
-	Body.$inject = [ '$scope', '$state','$window', '$log', '$timeout'];
+	Body.$inject = [ '$scope', '$state', '$window', '$log', '$timeout'];
 
 	function Body($scope, $state, $window, $log, $timeout) {
 
         var vm = this;
         vm.showMenu = false;
-
-        $scope.open = false;
 
         /**
 		 * What happens when a user hovers over tab.
@@ -25,7 +23,30 @@
          * @param event
          */
         vm.leaveTab = function(event) {
-        	$log.info(event);
+        	//console.dir(event);
+            var element, pX, pY, eX, eY, eH, eW;
+            pX = event.clientX;
+            pY = event.clientY;
+            element = jQuery(event.currentTarget);
+            eX = element.prop('offsetLeft');
+            eY = element.prop('offsetTop');
+            eH = element.prop('offsetHeight');
+            eW = element.prop('offsetWidth');
+            console.log(pX);
+            console.log(pY);
+            console.log(eX);
+            console.log(eY);
+            console.log(eH);
+            console.log(eW);
+            console.log(event);
+            console.log(element);
+            console.log("pointer: " + pY);
+            console.log("element: " + (eY + eH));
+            if (pY > (eY + eH)) {
+            	vm.setHover(true);
+			} else {
+            	vm.setHover(false);
+			}
 		};
 
         /**
@@ -33,12 +54,30 @@
          * @param event
          */
         vm.enterTab = function(event) {
-        	if (vm.timeout) {
-        		$timeout.cancel(vm.timeout);
-			}
-			vm.timeout = $timeout(function() {
-				$log.info(event.clientX + ", " + event.clientY);
-			}, 200);
+            var element, pX, pY, eX, eY, eH, eW;
+            pX = event.clientX;
+            pY = event.clientY;
+            element = jQuery(event.currentTarget);
+            eX = element.prop('offsetLeft');
+            eY = element.prop('offsetTop');
+            eH = element.height();
+            eW = element.width();
+            console.log(pX);
+            console.log(pY);
+            console.log(eX);
+            console.log(eY);
+            console.log(eH);
+            console.log(eW);
+            console.log(event);
+            console.log(element);
+            vm.setHover(true);
+            // if (vm.timeout) {
+        		// $timeout.cancel(vm.timeout);
+			// }
+			// vm.timeout = $timeout(function() {
+			// 	$log.info(event.clientX + ", " + event.clientY);
+            //
+			// }, 200);
 		};
 
 		function render()
