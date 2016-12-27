3a22e54f89107f3a182dfd8ecce2d9e145f1a6c5
matt <matt.ledbetter@gmail.com>
2016-12-27 18:26:57 -0500

    fixed menu open and leave methods.

--- src/main/resources/static/app/assets/controller/body.controller.js
+++ src/main/resources/static/app/assets/controller/body.controller.js
@@ -9,6 +9,7 @@
 
         var vm = this;
         vm.showMenu = false;
+        vm.showSubmenu = false;
 
         /**
 		 * What happens when a user hovers over tab.
@@ -23,28 +24,30 @@
          * @param event
          */
         vm.leaveTab = function(event) {
-        	//console.dir(event);
-            var element, pX, pY, eX, eY, eH, eW;
-            pX = event.clientX;
-            pY = event.clientY;
-            element = jQuery(event.currentTarget);
-            eX = element.prop('offsetLeft');
-            eY = element.prop('offsetTop');
+        	console.dir(event);
+            var element, pX, pY, eX, eY, eH, eW, leavingLeft, leavingRight, leavingTop, leavingBottom;
+            pX = event.pageX;
+            pY = event.pageY;
+            element = angular.element(event.currentTarget);
+            eX = event.target.getBoundingClientRect().left;
+            eY = event.target.getBoundingClientRect().top;
             eH = element.prop('offsetHeight');
             eW = element.prop('offsetWidth');
-            console.log(pX);
-            console.log(pY);
-            console.log(eX);
-            console.log(eY);
-            console.log(eH);
-            console.log(eW);
-            console.log(event);
-            console.log(element);
-            console.log("pointer: " + pY);
-            console.log("element: " + (eY + eH));
-            if (pY > (eY + eH)) {
+            leavingLeft = pX <= eX;
+            leavingRight = pX >= (eX + eW);
+            leavingBottom = pY >= (eY + eH);
+            leavingTop = pY <= eY;
+            if (pY >= (eY + eH)) { // if pointer Y pos is gt (elem y + elem height)
+                console.log("left: " + leavingLeft);
+                console.log("right: " + leavingRight);
+                console.log("bottom: " + leavingBottom);
+                console.log("top: " + leavingTop);
             	vm.setHover(true);
 			} else {
+                console.log("left: " + leavingLeft);
+                console.log("right: " + leavingRight);
+                console.log("bottom: " + leavingBottom);
+                console.log("top: " + leavingTop);
             	vm.setHover(false);
 			}
 		};
@@ -62,23 +65,12 @@
             eY = element.prop('offsetTop');
             eH = element.height();
             eW = element.width();
-            console.log(pX);
-            console.log(pY);
-            console.log(eX);
-            console.log(eY);
-            console.log(eH);
-            console.log(eW);
-            console.log(event);
-            console.log(element);
             vm.setHover(true);
-            // if (vm.timeout) {
-        		// $timeout.cancel(vm.timeout);
-			// }
-			// vm.timeout = $timeout(function() {
-			// 	$log.info(event.clientX + ", " + event.clientY);
-            //
-			// }, 200);
 		};
+
+        vm.leaveSubmenu = function(event) {
+            vm.setHover(false);
+        };
 
 		function render()
 		{
