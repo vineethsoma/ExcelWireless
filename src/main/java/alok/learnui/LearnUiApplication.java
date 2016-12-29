ede91ebbc3c39775d74209e94ac38a40cacedca7
asp5045 <alok_s_patel1@homedepot.com>
2016-12-28 19:22:12 -0500

    test

--- src/main/java/alok/learnui/LearnUiApplication.java
+++ src/main/java/alok/learnui/LearnUiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnUiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnUiApplication.class, args);
+		SpringApplication.run(LearnUiApplication.class, argss);
 	}
 }
