af9714163fef7c15798db6e82df03389ae874197
matt <matt.ledbetter@gmail.com>
2016-12-31 11:00:32 -0500

    hava brand menu and category menus dynamically showing.

--- src/main/resources/static/assets/controller/body.controller.js
+++ src/main/resources/static/assets/controller/body.controller.js
@@ -1,32 +1,49 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular.module('sampleApp').controller('BodyController', Body);
+    angular.module('sampleApp').controller('BodyController', Body);
 
-	Body.$inject = [ '$scope', '$state', '$window', '$log', '$timeout', 'dataService'];
+    Body.$inject = ['$scope', '$state', '$window', '$log', '$timeout', 'dataService'];
 
-	function Body($scope, $state, $window, $log, $timeout, dataService) {
+    function Body($scope, $state, $window, $log, $timeout, dataService) {
 
         var vm = this;
         vm.showMenu = false;
         vm.showSubmenu = false;
         vm.service = dataService;
         vm.categoryList = [];
-        vm.mock =
+        vm.brandList = [];
+        vm.dataList = [];
+        vm.responseData = {};
+
+        vm.setDataList = function (name) {
+            vm.dataList = [];
+            if (name === 'category') {
+                vm.categoryList.forEach(function (item, index) {
+                    vm.dataList.push({"id": item.categoryId, "name": item.categoryName});
+                });
+            } else if (name === 'brand') {
+                vm.brandList.forEach(function (item, index) {
+                    vm.dataList.push({ "id": item.brandId, "name": item.brandName, "modelList": item.modelDtoList })
+                });
+            }
+        };
+
+
         /**
-		 * What happens when a user hovers over tab.
+         * What happens when a user hovers over tab.
          * @param isHover
          */
-        vm.setHover = function(isHover) {
+        vm.setHover = function (isHover) {
             vm.showMenu = isHover;
         };
 
         /**
-		 * What happens when a user mouse leaves a tab with a submenu.
+         * What happens when a user mouse leaves a tab with a submenu.
          * @param event
          */
-        vm.leaveTab = function(event) {
-        	console.dir(event);
+        vm.leaveTab = function (event) {
+            //console.dir(event);
             var element, pX, pY, eX, eY, eH, eW, leavingLeft, leavingRight, leavingTop, leavingBottom;
             pX = event.pageX;
             pY = event.pageY;
@@ -40,18 +57,19 @@
             leavingBottom = pY >= (eY + eH);
             leavingTop = pY <= eY;
             if (pY >= (eY + eH)) { // if pointer Y pos is gt (elem y + elem height)
-            	vm.setHover(true);
-			} else {
-            	vm.setHover(false);
-			}
-		};
+                vm.setHover(true);
+            } else {
+                vm.setHover(false);
+            }
+        };
 
         /**
-		 * What happens when a user mouse enters a tab with a submenu.
+         * What happens when a user mouse enters a tab with a submenu.
          * @param event
          */
-        vm.enterTab = function(event) {
+        vm.enterTab = function (tabName, event) {
             var element, pX, pY, eX, eY, eH, eW;
+            vm.showSubmenu = false;
             pX = event.clientX;
             pY = event.clientY;
             element = jQuery(event.currentTarget);
@@ -59,27 +77,37 @@
             eY = element.prop('offsetTop');
             eH = element.height();
             eW = element.width();
+            if (tabName === 'category') {
+                vm.setDataList('category');
+            } else if (tabName === 'brand') {
+                vm.setDataList('brand');
+            }
             vm.setHover(true);
-		};
 
-        vm.leaveSubmenu = function(event) {
+        };
+
+        vm.leaveSubmenu = function (event) {
             vm.setHover(false);
         };
 
-        vm.subMenuButtonEnter = function(event) {
+        vm.subMenuButtonEnter = function (event) {
+            console.log(event);
             vm.showSubmenu = true;
         };
 
-        vm.leaveSubSubmenu = function(event) {
+        vm.leaveSubSubmenu = function (event) {
             vm.showSubmenu = false;
         };
 
-		function render()
-		{
+        function render() {
             vm.service.callGenericMethod('/getWebMenu', 'GET', {}).then(
                 function success(response) {
-                    if (response.data && response.data.categoryDtoList) {
+                    if (response.data &&
+                        response.data.categoryDtoList &&
+                        response.data.webBrandDtoList) {
                         vm.categoryList = response.data.categoryDtoList;
+                        vm.brandList = response.data.webBrandDtoList;
+                        $log.info(response);
                     } else {
                         $log.$error("Category response is missing the categoryDtoList.  Unable to process request.", response);
                     }
@@ -89,8 +117,8 @@
 
                 }
             )
-		}
+        }
 
-		render();
-	}
+        render();
+    }
 })();
