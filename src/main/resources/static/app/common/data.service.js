a78f55f5a3fface8bf38bf273d1cbbfa675790b1
MXL4972 <matthew_ledbetter@homedepot.com>
2017-01-11 18:18:20 -0500

    adding services.

---
+++ src/main/resources/static/app/common/data.service.js
@@ -0,0 +1,43 @@
+(function() {
+    angular.module('excelWireless')
+        .factory("dataService", dataService);
+
+    dataService.$inject = ['$http', '$log'];
+
+    function dataService($http, $log) {
+        var service = {}, cache = false, vm = this;
+        vm.domain = 'http://localhost';
+        vm.port = '8080';
+        vm.defaultMethodPath = '/getWebMenu';
+
+        service.callGenericMethod = function (methodPath, methodType, data, headers, successCallback, errorCallback) {
+            var _headers = headers ? headers : {"Content-Type": "application/json"},
+                _domain = vm.domain,
+                _port = (vm.port && vm.port.length > 0) ? (":" + vm.port) : "",
+                _path = methodPath || vm.defaultMethodPath,
+                _methodType = (methodType === "GET" || methodType === "PUT" || methodType === "POST" || methodType === 'PATCH' || methodType === "DELETE") ? methodType : "GET",
+                _successCallback, _errorCallback;
+
+            function successMethod(response){
+                return response;
+            }
+
+            function errorMethod(reason) {
+                $log.error("called default error method: {0}", reason);
+                return reason;
+            }
+
+            _successCallback = (typeof successCallback === 'function') ? successCallback : successMethod;
+            _errorCallback = (typeof errorCallback === 'function') ? errorCallback : errorMethod;
+            return $http({
+                cache: cache,
+                data: data ? data : {},
+                headers: _headers,
+                method: _methodType,
+                url: _domain + _port + _path
+            }).then(_successCallback, _errorCallback);
+        };
+
+        return service;
+    }
+}());
---
+++ src/main/resources/static/app/common/productDetails.service.js
@@ -0,0 +1,25 @@
+(function() {
+    angular.module('sampleApp')
+        .factory("productDetailsService", productDetailsService);
+
+    productDetailsService.$inject = ['dataService'];
+
+    function productDetailsService(dataService) {
+        var vm = this;
+        vm.genericService = dataService;
+        vm.productDetailsEndpoint = "/getProductsByCategory?category_Id=";
+
+        /**
+         * getProductByCategoryId
+         * @param catId
+         */
+        function getProductByCategory(catId) {
+            var path = vm.productDetailsEndpoint + catId;
+            return vm.genericService.callGenericMethod(path,'GET', {});
+        }
+
+        return {
+            getProdByCatId: getProductByCategory
+        }
+    }
+}());
