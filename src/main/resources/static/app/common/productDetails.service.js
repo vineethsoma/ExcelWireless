170c05e7d144b2a6f3387d9aaeb8ca05a221522b
MXL4972 <matthew_ledbetter@homedepot.com>
2017-01-11 18:29:05 -0500

    added back  services.

--- src/main/resources/static/app/common/productDetails.service.js
+++ src/main/resources/static/app/common/productDetails.service.js
@@ -1,5 +1,5 @@
 (function() {
-    angular.module('sampleApp')
+    angular.module('excelWireless')
         .factory("productDetailsService", productDetailsService);
 
     productDetailsService.$inject = ['dataService'];
