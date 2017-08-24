e908f13fc577ddcf6fb4f371ae927b63eb0e6369
Vineeth Soma <vineethsoma@gmail.com>
2017-08-24 10:29:45 -0400

    added search widget

--- client/src/app/product/product.module.ts
+++ client/src/app/product/product.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchWidgetComponent } from './search-widget/search-widget.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductRoutingModule
   ],
-  declarations: [SearchResultsComponent]
+  declarations: [SearchResultsComponent, SearchWidgetComponent]
 })
 export class ProductModule { }
