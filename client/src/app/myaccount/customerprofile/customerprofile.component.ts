9aee1fc010e7b88a6c4095ebb23a0ef88a24cf5c
apatel2 <alok.patel@coxautoinc.com>
2017-08-29 18:54:36 -0400

    finally fixed the design issue with home page moddel design

--- client/src/app/myaccount/customerprofile/customerprofile.component.ts
+++ client/src/app/myaccount/customerprofile/customerprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../user.service";
 
 @Component({
   selector: 'app-customerprofile',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerprofileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  getCusr() {
+
   }
 
 }
